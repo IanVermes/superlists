ab3c17e06345ee1c21b9d2d4c4a641f741c8a132
Ian Vermes <ian.vermes@gmail.com>
2018-01-31 14:43:13 +0000

    Added if __name__ == "__main__" block

--- functional_test.py
+++ functional_test.py
@@ -5,6 +5,18 @@ from selenium import webdriver
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
+    """Container for user orientated test conditions. My first unittest!
+
+    Methods starting with test will be invoked by the test runner.
+
+    self.setUp() and self.tearDown() are special methods which get run before and
+    afer each test.
+
+    https://docs.python.org/3/library/unittest.html#unittest.TestCase
+    This class inherits from TestCase. TestCase provides some helpful methods that replace things like assert.
+    e.g. assertIn(a, b) or assertEquals(a, b) or assertLess(a, b)
+
+    """
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -15,16 +27,16 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         """User story: User visits site, adds items to list and checks they persist.
 
-        What is a USER STORY - By framing a coherent story as comments we always test from the
-        point of view of the user.
-        """
+        What is a USER STORY? By framing a coherent story as comments we always test
+        from the point of view of the user."""
+
         # Edith has heard about a cool new online to-do app. She goes to check it out
         # its homepage.
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-do', self.browser.title)
-        self.fail("Finish the test!")
+        self.fail("Finish coding your test!")
 
         # She is invited to enter a to-do item straight away.
 
@@ -47,3 +59,7 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisified she goes back to sleep.
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
