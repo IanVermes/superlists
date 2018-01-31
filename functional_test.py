70d889c0a6f6f269d2572ada2c71f9f0a2b4d697
Ian Vermes <ian.vermes@gmail.com>
2018-01-31 23:33:59 +0000

    Functional test now checks repeated user input

--- functional_test.py
+++ functional_test.py
@@ -92,19 +92,29 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == "1: Buy peacock feathers" for row in rows),
-            "New to-do item didn't appear in table."
+            f"New to-do item didn't appear in table. Contents were:\n{table.text}"
         )
 
         # There is still a text box inviting her to add another item.
-        self.fail('Finish writing your func test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
 
         # She enters "Use peacock feathers to make a fly".
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again and shows both items on her list.
+        table = self.browser.find_element_by_id('id_new_item')
+        rows = table.find_element_by_tag_name('tr')
+        row_texts = [row.text for row in rows]
+        self.assertIn("1: Buy peacock feathers", row_texts)
+        self.assertIn("2: Use peacock feathers to make a fly", row_texts)
+
 
         # Edith wonders whether the site will remember her list. Then she sees that the
         # site has generate a unique URL for her -- there is some explanatory text to
         # that effect.
+        self.fail('Finish writing your func test!')
 
         # She visits that URL - her to-do list is still there.
 
