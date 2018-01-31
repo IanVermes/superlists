2566821309d2632c1ddc1aa9a7b3b00cf9393c99
Ian Vermes <ian.vermes@gmail.com>
2018-01-31 23:43:44 +0000

    Refactor functional test with helper method

--- functional_test.py
+++ functional_test.py
@@ -57,6 +57,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, comparator_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(comparator_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """User story: User visits site, adds items to list and checks they persist.
 
@@ -87,13 +92,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list.
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)  # There is a better wait-for-page-load that I've seen on StackOverflow.
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows),
-            f"New to-do item didn't appear in table. Contents were:\n{table.text}"
-        )
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item.
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -104,11 +103,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again and shows both items on her list.
-        table = self.browser.find_element_by_id('id_new_item')
-        rows = table.find_element_by_tag_name('tr')
-        row_texts = [row.text for row in rows]
-        self.assertIn("1: Buy peacock feathers", row_texts)
-        self.assertIn("2: Use peacock feathers to make a fly", row_texts)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
 
         # Edith wonders whether the site will remember her list. Then she sees that the
