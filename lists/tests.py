5d1e8f8192646f21340850782ba0530a22167445
Ian Vermes <ian.vermes@gmail.com>
2018-02-01 14:25:17 +0000

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@
 from django.test import TestCase
 
 from lists.views import home_page
+from lists.models import Item
 
 # Create your tests here.
 
@@ -36,3 +37,29 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+    """Unit tests for interacting with Django ORM.
+
+    ORM is the abstraction for a database of tables.
+        Classes map to tables.
+        Attributes map to columns.
+        An instance of a class is a row(s).
+    """
+
+    def test_saving_and_retrieving_items(self):
+        """Unit test for ORM saving and retrieving objects."""
+        first_item = Item()
+        first_item.text = 'The first (ever) list item.'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item.')
+        self.assertEqual(second_saved_item.text, 'Item the second')
