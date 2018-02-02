ff2365c385a873d4dd13370eb3299e4244ff41d2
Ian Vermes <ian.vermes@gmail.com>
2018-02-02 12:18:52 +0000

    URL added '/lists/new', redirects to custom URL

--- lists/tests.py
+++ lists/tests.py
@@ -86,18 +86,17 @@ class ListViewTest(TestCase):
         self.assertContains(response, string2)
 
 
-class NewLitstTest(TestCase):
+class NewListsTest(TestCase):
         def test_can_save_a_POST_request(self):
             "Unit test for view function checking POST value is added to DB."
-            response = self.client.post('/', data={'item_text': 'A new list item'})
-
+            self.client.post('/lists/new', data={'item_text': 'A new list item'})
             self.assertEqual(Item.objects.count(), 1)
+
             new_item = Item.objects.first()
+
             self.assertEqual(new_item.text, 'A new list item')
 
         def test_redirects_after_POST(self):
             "Unit test that checks view function redirects after POST"
-            response = self.client.post('/', data={'item_text': 'A new list item'})
-            
-            self.assertEqual(response.status_code, 302)
-            self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+            response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+            self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
