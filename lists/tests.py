6f14316bc847661478cad1daaca42397f8af0f8b
Ian Vermes <ian.vermes@gmail.com>
2018-02-02 13:05:58 +0000

    List model + DB migrations but View test regression

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@
 from django.test import TestCase
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 
@@ -32,7 +32,7 @@ class HomePageTest(TestCase):
         response = self.client.get('/')  # View func generetes a response.
         self.assertTemplateUsed(response, 'home.html')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     """Unit tests for interacting with Django ORM.
 
     ORM is the abstraction for a database of tables.
@@ -43,20 +43,31 @@ class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
         """Unit test for ORM saving and retrieving objects."""
+        _list = List()
+        _list.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item.'
+        first_item.list = _list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = _list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, _list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item.')
+        self.assertEqual(first_saved_item.list, _list)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, _list)
 
 
 class ListViewTest(TestCase):
